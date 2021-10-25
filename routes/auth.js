e8e2a53f7f5a7813c7e50fc662144704191b8613
Sergei Sokolov <sokolov12ssa@gmail.com>
2021-10-25 19:41:48 +0500

    input validation

--- routes/auth.js
+++ routes/auth.js
@@ -9,4 +9,4 @@ router.get('/', (req, res) =>res.send('Get logged in user'))
 // @ddesc Auth user and get token
 // @access Public
 router.post('/', (req, res) => res.send('Logged in user'))
-module.exports = router
+module.exports = router 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const {check, validationResult} = require('express-validator')
+const { ResultWithContext } = require('express-validator/src/chain')
+const User = require('../models/User')
+
 //@route POST api/users
 // @ddesc Registering a user
 // @access Public
-router.post('/', (req, res) => res.send('Register a user'))
+router.post('/', [check('name', 'Please enter your name').not().isEmpty(),
+                  check('email', 'Please include a valid email').isEmail(),
+                  check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})], (req, res) => {
+                      const errors = validationResult(req)
+                      if(!errors.isEmpty()) {
+                         return res.status(400).json({errors: errors.array()})
+                      }
+                      res.send('Passed')
+                      
+                  })
 module.exports = router
