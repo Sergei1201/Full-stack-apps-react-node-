be588b53822f92c9a47959fba5457df8d818d13a
Sergei Sokolov <sokolov12ssa@gmail.com>
2021-11-02 20:50:06 +0500

    Creating client folder and configuring

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Fragment } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Home from './components/pages/Home';
+import {Home} from './components/pages/Home';
 import About from './components/pages/About';
 
 
