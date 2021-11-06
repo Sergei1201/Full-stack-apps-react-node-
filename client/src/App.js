5dee235b54ad492063ec93a4583d366117d3dd99
Sergei Sokolov <sokolov12ssa@gmail.com>
2021-11-06 18:54:32 +0500

    Adding contacts to the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Fragment } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ContactState from './context/contact/ContactState';
 import Navbar from './components/layout/Navbar'
-import {Home} from './components/pages/Home';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 
 const App = () => {
   return (
+    <ContactState>
     <Router>
     <Fragment>
       <Navbar/>
@@ -16,11 +18,13 @@ const App = () => {
         <Route exact path = '/' component = {Home}/>
         <Route exact path = '/about' component = {About}/>
      
-      </Switch>
+      </Switch> 
     </div>
       </Fragment>
     </Router>
+    </ContactState>
   );
+  
 }
 
 export default App;
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,83 @@
+import { useReducer } from "react";
+import { v4 as uuidv4 } from 'uuid'
+import ContactContext from "./contactContext";
+import contactReducer from './contactReducer'
+import {ADD_CONTACT,
+        DELETE_CONTACT,
+        SET_CURRENT,
+        CLEAR_CURRENT,
+        UPDATE_CONTACT,
+        FILTER_CONTACTS,
+        CLEAR_FILTER} from '../types'
+
+// Creating initial state
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "John@gmail.com",
+                phone: '111-222',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: "Jane Doe",
+                email: "Jane@gmail.com",
+                phone: '111-322',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: "Jimm Dane",
+                email: "Jimm@gmail.com",
+                phone: '231-232',
+                type: 'professional'
+            },
+
+        ]
+    }
+
+    // Pulling out the state and dispatching the reducer
+    const [state, dispatch] = useReducer(contactReducer, initialState) 
+
+    // Having all the necessary actions
+    
+    // Add contact
+ 
+    const addContact = (contact) => {
+        // Generating a "fake" ID for a contact before dealing with MongoDB (After connecting to the backend, we
+        // will be using data from the database that provides us with a built-in ID)
+        contact.id = uuidv4()
+        dispatch({type: ADD_CONTACT, payload: contact}) 
+    }
+
+    // Delete contact
+
+    // Set current contact
+
+    // Clear current contact 
+
+    // Update the contact
+
+    // Filter contact
+
+    // Clear filter
+
+    return (
+        <ContactContext.Provider
+        value = {{
+            contacts: state.contacts,
+            addContact
+
+        }}>
+            {props.children}
+      
+
+        </ContactContext.Provider>  
+    )
+
+}
+
+export default ContactState
